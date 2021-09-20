b84fb397761a33fd07355eeb8221eaa151ec2de2
apple <applezyh@outlook.com>
2021-09-20 21:58:47 +0800

    fix bug

--- asm/test.s
+++ asm/test.s
@@ -10,8 +10,6 @@ main:
     sub sp, sp, #4
     movw r0, #57600
     movt r0, #1525
-    mov r1, #0
-    sub r0, r1, r0
     str r0, [sp, #0]
     ldr r0, [sp, #0]
     push {r0}
@@ -19,9 +17,6 @@ main:
     add sp, sp, #4
     bl newline
     add sp, sp, #4
-    pop {r0}
-    mov r1, #0
-    push {r1}
-    mov pc, r0
+    pop {pc}
 
-//cost 3.411000ms.
+//cost 0.318000ms.
--- test.c
+++ test.c
@@ -1,12 +1,12 @@
 void print(int n);
 int getint();
 void newline();
-int main()
+void main()
 {
-    int a=-100000000;
+    int a=100000000;
     print(a);
     newline();
-    return 0;
+    return;
 }
 /*这是第一次作业我要好好写
 1913656赵泳豪
